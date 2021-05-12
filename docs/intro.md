62ee06dad611a82705c13d2115ae36ea3648e201
Vic <orbonvictor@gmail.com>
2021-05-12 10:58:08 +0800

    first commit

---
+++ docs/intro.md
@@ -0,0 +1,11 @@
+---
+sidebar_position: 1
+---
+
+# Docs
+
+* API-and-Scaling-Docs
+* App-Usage-Manual
+* Contributing-Guidelines
+* LMS-Installation-and-Docs
+* User Access Role
---
+++ docusaurus.config.js
@@ -0,0 +1,93 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: 'Stock Knowledge',
+  tagline: 'Play to Learn, learn to play!',
+  url: 'https://VicOrbonII.github.io',
+  baseUrl: '/Docusaurus-Documentation/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+   organizationName: 'VicOrbonII', // Usually your GitHub org/user name.
+  projectName: 'Docusaurus-Documentation', // Usually your repo name.
+  themeConfig: {
+    navbar: {
+      title: 'Stock Knowledge',
+      logo: {
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
+        
+        
+      },
+     
+    },
+    footer: {
+      
+      style: 'light',
+      links: [
+       
+        {
+          title: 'Community',
+          items: [
+            
+            {
+              label: 'Facebook',
+              href: 'https://www.facebook.com/groups/1118893898545259?_rdc=1&_rdr',
+      
+              
+            },
+            {
+              href: 'https://github.com/Stock-Knowledge-Engineering',
+              label: 'GitHub',
+              
+              
+            },
+            {
+              href: 'https://twitter.com/StockKnowledge_',
+              label: 'Twitter',
+              
+              
+            },
+             {
+              
+              href: 'https://www.instagram.com/stockknowledgetool/?hl=en',
+              label: 'Instagram',
+              
+              
+            },
+            {
+              href: 'https://www.linkedin.com/company/stock-knowledge/mycompany',
+              label: 'LinkIn',
+            
+              
+            },
+           
+          ],
+        },
+        
+      ],
+      copyright: `Copyright © ${new Date().getFullYear()} Stock Knowledge Corp. Built with Docusaurus.`,
+    },
+  },
+  presets: [
+    [
+      '@docusaurus/preset-classic',
+      {
+        docs: {
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
+        },
+        blog: {
+          showReadingTime: false,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
+        theme: {
+          customCss: require.resolve('./src/css/custom.css'),
+        },
+      },
+    ],
+  ],
+};
